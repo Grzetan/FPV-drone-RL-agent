fdd6428fb1b9f0caaa1c3bbeab1d15598de4f4e8
grzetan <grzetan@gmail.com>
2025-06-01 18:35:12 +0200

    Removed useless comments

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import numpy as np
 from PyFlyt.core import Aviary
 import cv2
 
-# Define starting positions and orientations
 start_pos = np.array([[0.0, 0.0, 0.0]])
 start_orn = np.array([[0.0, 0.0, 0.0]])
 
-# Instantiate aviary with rgb camera enabled
 env = Aviary(
     start_pos=start_pos,
     start_orn=start_orn,
@@ -15,23 +13,20 @@ env = Aviary(
     drone_options={"use_camera": True},
 )
 
-# Add a red sphere at position [2, 0, 1]
 sphere_visual_id = env.createVisualShape(
-    shapeType=env.GEOM_SPHERE, radius=0.1, rgbaColor=[1, 0, 0, 1]  # Red color
+    shapeType=env.GEOM_SPHERE, radius=0.1, rgbaColor=[1, 0, 0, 1]
 )
 sphere_id = env.createMultiBody(
-    baseMass=0,  # Static object
+    baseMass=0,
     baseVisualShapeIndex=sphere_visual_id,
     basePosition=[2, 0, 1],
 )
 
-# Define control mode
 env.set_mode(0)
 
-# Define a setpoint
 setpoint = np.array([0.0, 0.0, -1.57, 0.5])
 env.set_setpoint(0, setpoint)
-# Step the physics
+
 for i in range(10000):
     obs_dict = env.step()
     frame = env.drones[0].rgbaImg
